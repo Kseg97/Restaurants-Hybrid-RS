6b419d6b7f8e4105bb305a8ece03db7dd1e4c61a
Camilo Segura <camilosegura6@gmail.com>
2019-08-06 18:54:29 -0500

    First commit

---
+++ restaurants.py
@@ -0,0 +1,81 @@
+import numpy as np
+import scipy
+import pandas as pd
+import math
+import random
+import sklearn
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse.linalg import svds
+import matplotlib.pyplot as plt
+
+# IMPORT AND DROP
+# Restaurants
+restaurants_df = pd.read_csv('./geoplaces2.csv')
+restaurants_df.set_index(['placeID']).drop(columns=['the_geom_meter','address','country','fax','zip','smoking_area','url','Rambience','franchise','area','other_services'])
+res_accepted_pay = pd.read_csv('./chefmozaccepts.csv')
+res_parking = pd.read_csv('./chefmozparking.csv')
+# Users
+user_df = pd.read_csv('./userprofile.csv')
+user_df.set_index(['userID']).drop(columns=['smoker','ambience','hijos','marital_status','birth_year','interest','personality','religion','activity','color','weight','height'])
+usr_payment = pd.read_csv('./userpayment.csv')
+# Ratings
+# (userID, placeID, rating)
+ratings_df = pd.read_csv('./rating_final.csv')
+ratings_df = ratings_df.drop(columns=['food_rating', 'service_rating'])
+
+# MAPPING AND ENCODING (OHE)
+
+# User Payment
+# {userID*, PAYMENT_cash  PAYMENT_credit_card  PAYMENT_debit_card}
+map_user_payment = {
+   'cash': 'cash',
+   'bank_debit_cards': 'debit_card', 
+   'MasterCard-Eurocard': 'credit_card', 
+   'VISA': 'credit_card',
+   'American_Express': 'credit_card',  
+}
+usr_payment['payment'] = usr_payment['Upayment'].apply(lambda x: map_user_payment[x])
+usr_payment = usr_payment[['userID']].join(pd.get_dummies(usr_payment['payment']).add_prefix('PAYMENT_')).groupby('userID').max()
+
+# Restaurants
+map_restaurant_payment = {
+   'cash': 'cash',
+   'bank_debit_cards': 'debit_card', 
+   'MasterCard-Eurocard': 'credit_card', 
+   'VISA': 'credit_card',
+   'Visa': 'credit_card',
+   'American_Express': 'credit_card',  
+   'Japan_Credit_Bureau': 'credit_card',  
+   'Carte_Blanche': 'credit_card',
+   'Diners_Club': 'credit_card', 
+   'Discover': 'credit_card', 
+   'gift_certificates': 'other',  
+   'checks': 'other',  
+}
+res_accepted_pay['payment'] = res_accepted_pay['Rpayment'].apply(lambda x: map_restaurant_payment[x])
+res_accepted_pay = res_accepted_pay[['placeID']].join(pd.get_dummies(res_accepted_pay['payment']).add_prefix('PAYMENT_')).groupby('placeID').max()
+
+map_restaurant_parking = {
+   'none': 'no',
+   'public': 'yes', 
+   'yes': 'yes', 
+   'valet parking': 'yes',
+   'fee': 'yes',
+   'street': 'street',  
+   'validated parking': 'yes',  
+}
+res_parking['parking'] = res_parking['parking_lot'].apply(lambda x: map_restaurant_parking[x])
+res_parking = res_parking[['placeID']].join(pd.get_dummies(res_parking['parking']).add_prefix('PARKING_')).groupby('placeID').max()
+
+# MERGE
+# Users
+# {userID*, 'latitude', 'longitude', 'drink_level', 'dress_preference', 'transport', 'budget', 'PAYMENT_cash', 'PAYMENT_credit_card', 'PAYMENT_debit_card'}
+user_df = pd.merge(user_df, usr_payment, how='left', on=['userID'])
+# Restaurants
+# 
+restaurants_df = pd.merge(restaurants_df, res_accepted_pay, how='left', on=['placeID'])
+restaurants_df = pd.merge(restaurants_df, res_parking, how='left', on=['placeID'])
+# At this point, ratings_df, user_df and restaurants_df are clean
